bd8ae179e66b2692fd57390c36dd240acd472505
siswatelkom07 <140671201+siswatelkom07@users.noreply.github.com>
2024-01-09 20:49:10 +0700

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Bus {
+    private int penumpang,maxpenumpang;
+    
+    // konstruktor
+    public Bus(int maxpenumpang){
+        this.maxpenumpang=maxpenumpang;
+        penumpang = 0;
+    }
+    
+    //method mutator 
+    public void pluspenumpang(int penumpang){
+        int temp;
+        temp=this.penumpang+penumpang;
+        if (temp>=maxpenumpang){
+            System.out.println("Overload penumpang");
+        }
+        else {
+            this.penumpang=temp;
+        }
+        
+    }
+    public void cetak(){
+        System.out.println("Penumpang sekarang = "+penumpang);
+        System.out.println("penumpang seharusnya adalah ="+maxpenumpang);
+    }
+}
+
+
---
+++ UjiBus.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+ public class UjiBus {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bus busMini=new Bus(10);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(3);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(1);
+        busMini.cetak();
+        
+        busMini.pluspenumpang(1);
+        busMini.cetak();
+        
+          
+    }
+}
